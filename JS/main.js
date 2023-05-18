41d212878b6c2c2fae36c16e36db892d77a5dc9f
Yuri <57808056+Naneek-code@users.noreply.github.com>
2023-05-18 02:40:47 -0300

    desabilita F12 e cliques direitos

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12") {
+    e.preventDefault();
+  }
+});
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+  },
+  false
+);
