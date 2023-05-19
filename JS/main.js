2c427de2fa0198cfeb59cd38a313ec527e037d6b
Yuri <57808056+Naneek-code@users.noreply.github.com>
2023-05-19 18:38:06 -0300

    login required system

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,12 @@
-document.addEventListener("keydown", function (e) {
-  if (e.key === "F12") {
-    e.preventDefault();
+const UserPss = document.getElementById("user_pss");
+const UserLogin = document.getElementById("user_id");
+const loginOn = document.querySelector(".login_btn");
+const login = document.getElementById("form_login");
+
+login.addEventListener("input", () => {
+  if (UserPss.value != "" && UserLogin.value != "") {
+    loginOn.classList.add("active");
+  } else {
+    loginOn.classList.remove("active");
   }
 });
-document.addEventListener(
-  "contextmenu",
-  function (e) {
-    e.preventDefault();
-  },
-  false
-);
