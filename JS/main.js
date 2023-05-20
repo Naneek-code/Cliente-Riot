ec546c7798ec4af66d8912e088cf7d937e70829d
Yuri <57808056+Naneek-code@users.noreply.github.com>
2023-05-20 03:19:38 -0300

    Buttons for close and open

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,30 @@ const UserPss = document.getElementById("user_pss");
 const UserLogin = document.getElementById("user_id");
 const loginOn = document.querySelector(".login_btn");
 const login = document.getElementById("form_login");
+const clientButtons = document.querySelector(".btns_client");
+const riotInk = document.querySelector(".RIOT");
+const riotClient = document.querySelector(".RiotClient");
 
-login.addEventListener("input", () => {
-  if (UserPss.value != "" && UserLogin.value != "") {
+if (UserPss && UserLogin) {
+  UserPss.addEventListener("input", toggleLoginButtonState);
+  UserLogin.addEventListener("input", toggleLoginButtonState);
+}
+
+function toggleLoginButtonState() {
+  if (UserPss.value !== "" && UserLogin.value.trim() !== "") {
     loginOn.classList.add("active");
   } else {
     loginOn.classList.remove("active");
   }
-});
+}
+
+riotInk.addEventListener("dblclick", toggleClientState);
+clientButtons.addEventListener("click", toggleClientState);
+
+function toggleClientState() {
+  if (riotClient.classList.contains("closed")) {
+    riotClient.classList.remove("closed");
+  } else {
+    riotClient.classList.add("closed");
+  }
+}
